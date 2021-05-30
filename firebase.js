69305f70d173179f3279825a06c0da0f487babed
Avi-Gamliel <76016795+Avi-Gamliel@users.noreply.github.com>
2021-05-31 01:18:44 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCeLA27coCGKTz0cmVhu0raoLUbugZX1BE",
-    authDomain: "track-your-electricity.firebaseapp.com",
-    projectId: "track-your-electricity",
-    storageBucket: "track-your-electricity.appspot.com",
-    messagingSenderId: "522719535763",
-    appId: "1:522719535763:web:89b884dd8cbf100a59e366"
+    apiKey: "****",
+    authDomain: "****",
+    projectId: "****",
+    storageBucket: "****",
+    messagingSenderId: "****",
+    appId: "****"
 };
 let app;
 
